661aa3b2d45a55fba6c001b7176224a2b29705ef
SeongHo5356 <65662139+SeongHo5356@users.noreply.github.com>
2020-06-29 20:28:29 +0900

    Add files via upload
    It's My Soultion of Lab#01, Lab#02 
    2019spring OOP

---
+++ Lab01_02/Lab01_02/basic2_Sizeof().cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+//sizeof()함수는 자료형의 크기를 알려주는 함수이다.
+int main() {
+	unsigned short siX; // (int) 생략 가능
+	unsigned iX;
+	long liX;
+	long long lliX;
+	cout << "sizeof(siX):" << sizeof(siX) << endl;
+	cout << "sizeof(iX):" << sizeof(iX) << endl;
+	cout << "sizeof(liX):" << sizeof(liX) << endl;
+	cout << "sizeof(lliX):" << sizeof(lliX) << endl;
+}
